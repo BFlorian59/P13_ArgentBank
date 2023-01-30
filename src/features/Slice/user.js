c586a27f584f7a8c33db93b956b4cdd6bd9e3610
BFlorian59 <florianbroeks59@gmail.com>
2023-01-30 17:12:10 +0100

    update user

--- src/features/Slice/user.js
+++ src/features/Slice/user.js
@@ -65,61 +65,17 @@ const { actions , reducer} = createSlice({
         reset: {
             reducer: () => initialState
         },
+        userUpdate:{
+            prepare: (token, firstName, lastName)=>({
+                payload: {token, firstName, lastName },
+            }),
+            reducer: (draft, action) =>{
+                draft.data.lastName = action.payload.lastName
+                draft.data.lastName = action.payload.lastName
+                return
+            }
+        }
     }
 })
 export {actions}
 export default reducer
-
-// const FETCHING = 'user/login/fetching'
-// const RESOLVED = 'user/login/resolved'
-// const REJECTED = 'user/login/rejected'
-
-// const userFetching = () => ({type: FETCHING})
-
-// const userResolved = (data) => ({type: RESOLVED, payload: data})
-
-// const userRejected = (error) => ({type: REJECTED, payload: error})
-
-// export default function userReducer(state = initialState, action){
-
-//     return produce(state, draft =>{
-//         switch (action.type) {
-//             case FETCHING:{               
-//                 if (draft.statusData === 'void') {
-//                     draft.statusData = 'pending'
-//                     return
-//                 }
-//                 if (draft.statusData === 'resolved') {
-//                     draft.statusData = 'updating'
-//                     return
-                    
-//                 }
-//                 if (draft.statusData === 'rejected') {
-//                     draft.error = null
-//                         draft.statusData = 'pending'
-//                         return
-//                 }
-//                 return
-//             }
-//             case RESOLVED:{ 
-//                 if (draft.statusData === 'pending' || draft.statusData === 'uptading') {
-//                     draft.data = action.payload
-//                     draft.statusData= 'resolved'
-//                     return
-//                 }
-//                 return
-//             }
-//             case REJECTED:{
-//                 if (draft.statusData === 'pending' || draft.statusData === 'uptading') {
-//                     draft.error = action.payload
-//                     draft.data = null 
-//                     draft.statusData= 'rejected'
-//                     return
-//                 }
-//                 return
-//             }
-//             default:
-//                 return
-//         }
-//     })
-// }
